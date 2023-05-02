58339b3d2deee837f4fd14032d519dae77e56341
Edilson Firmino <edfirmin@c2r5p2.42nice.fr>
2023-05-02 13:47:21 +0200

    oui

--- oui.c
+++ oui.c
@@ -1,5 +1,7 @@
 
 #include "mlx.h"
+#include "so_long.h"
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 
@@ -20,61 +22,107 @@ void	ft_putstr(char *str)
 	}
 }
 
+static int	size_str(int n)
+{
+	int	i;
+
+	i = 0;
+	if (n == -2147483648)
+	{
+		i = 11;
+		return (i);
+	}
+	if (n < 0)
+	{
+		n = -n;
+		i++;
+	}
+	while (n >= 10)
+	{
+		i++;
+		n = n / 10;
+	}
+	i++;
+	return (i);
+}
+
+char	*ft_itoa(int n)
+{
+	unsigned int	nbr;
+	int				i;
+	char			*str;
+
+	i = size_str(n);
+	if (n < 0)
+		nbr = -n;
+	else
+		nbr = n;
+	str = malloc(sizeof(char) * (i + 1));
+	if (str == 0)
+		return (0);
+	str[i--] = '\0';
+	while (i >= 0)
+	{
+		str[i] = nbr % 10 + '0';
+		nbr = nbr / 10;
+		i--;
+	}
+	if (n < 0)
+		str[0] = '-';
+	return (str);
+}
+
 int	touch(int key_touch, void *param)
 {
+	void	*mlx_ptr;
+
+	mlx_ptr = mlx_init();
+	if (key_touch == 53)
+		mlx_destroy_window(mlx_ptr, param);
 	param = NULL;
-	if (key_touch == 0)
+	if (key_touch == 0 || key_touch == 123)
 		ft_putstr("Gauche ");
-	if (key_touch == 13)
+	if (key_touch == 13 || key_touch == 126)
 		ft_putstr("Haut ");
-	if (key_touch == 2)
+	if (key_touch == 2 || key_touch == 124)
 		ft_putstr("Droite ");
-	if (key_touch == 1)
+	if (key_touch == 1 || key_touch == 125)
 		ft_putstr("Bas ");
-	return (key_touch);
+	return (1);
 }
 
 int main(void)
 {
-	void	*mlx_ptr;
-	void	*window; // pour recup. l'ID de la fenetre pour dessiner dedans plus tard
+	t_test	oui;
+	int		img_lo;
+	int		img_la;
+	int		lo;
+	int		la;
 	int		i;
 	int		j;
 
 	j = 0;
-	mlx_ptr = mlx_init();
-	window = mlx_new_window(mlx_ptr, 900, 500, "oui");
-	while (j <= 500)
+	oui.mlx_ptr = mlx_init();
+	oui.window = mlx_new_window(oui.mlx_ptr, 1024, 768, "oui");
+	oui.map = mlx_xpm_file_to_image(oui.mlx_ptr, "map.xpm", &img_lo, &img_la);
+	mlx_put_image_to_window(oui.mlx_ptr, oui.window, oui.map, 0, 0);
+	mlx_string_put(oui.mlx_ptr, oui.window, 925, 20, 0xFFFFFF, "Score:");
+	oui.perso = mlx_xpm_file_to_image(oui.mlx_ptr, "perso.xpm", &lo, &la);
+	mlx_put_image_to_window(oui.mlx_ptr, oui.window, oui.perso, 440, 200);
+	oui.rondin = mlx_xpm_file_to_image(oui.mlx_ptr, "rondin.xpm", &lo, &la);
+	while (j <= 768)
 	{
 		i = 0;
-		while (j <= 250 && i <= 900)
+		if (j == 0 || j == 768)
 		{
-			mlx_pixel_put(mlx_ptr, window, i, j, 0x004AFF);
-			i++;
+			while (i + 20 <= 1024)
+			{
+				mlx_put_image_to_window(oui.mlx_ptr, oui.window, oui.rondin, i, j);
+				i += 20;
+			}
 		}
-		while (j <= 290 && i <= 900)
-		{
-			mlx_pixel_put(mlx_ptr, window, i, j, 0xFFFFFF);
-			i++;
-		}
-		while (j <= 330 && i <= 900)
-		{
-			mlx_pixel_put(mlx_ptr, window, i, j, 0xFF0000);
-			i++;
-		}
-		while (j <= 370 && i <= 900)
-		{
-			mlx_pixel_put(mlx_ptr, window, i, j, 0xFFFFFF);
-			i++;
-		}
-		while (j <= 500 && i <= 900)
-		{
-			mlx_pixel_put(mlx_ptr, window, i, j, 0x004AFF);
-			i++;
-		}
-		j++;
+		else if (j != 0 || )
 	}
-	mlx_string_put(mlx_ptr, window, 450, 250, 0x004AFF, "667");
-	mlx_key_hook(window, touch, NULL);
-	mlx_loop(mlx_ptr);
+	mlx_hook(oui.window, 2, 0, touch, oui.window);
+	mlx_loop(oui.mlx_ptr);
 }
---
+++ so_long.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edfirmin <edfirmin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/01 15:16:44 by edfirmin          #+#    #+#             */
+/*   Updated: 2023/05/02 13:40:26 by edfirmin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+typedef struct s_test
+{
+	void	*mlx_ptr;
+	void	*window;
+	void	*map;
+	void	*perso;
+	void	*rondin;
+}				t_test;
+
+#endif
